fb5bcbb4862d9c23d9073faca43e39cde2a2574d
Chris Atkinson <chris@tkinson.net>
2023-10-25 12:45:48 -0400

    spelling.

--- honour-roll.php
+++ honour-roll.php
@@ -34,7 +34,7 @@ require_once( __DIR__ . '/Classes/MyClasses/autoloader.php' );
     }
     </style>
     <!-- sortable headers in tables -->
-    <script src="js/sorttable.js"></script>
+    <script src="js/sortable.js"></script>
   </head>
   <body>
     <div class="demo-layout mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
